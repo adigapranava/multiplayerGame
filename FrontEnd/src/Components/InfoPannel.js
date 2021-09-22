de4ed23937cbf03ca3c14748814e403d7b8e66a7
Pranava Adiga <66902872+adigapranava@users.noreply.github.com>
2021-09-22 17:02:10 +0530

    Update InfoPannel.js

--- FrontEnd/src/Components/InfoPannel.js
+++ FrontEnd/src/Components/InfoPannel.js
@@ -8,15 +8,21 @@ export default function InfoPannel({ player, setPlayer, socket ,room, setRoom })
     const [roomText, setRoomText] = useState("");
     const [createRoom, setCreateRoom] = useState(false);
     const [joinRoom, setJoinRoom] = useState(false);
-
+    
+    ////////// FORM HANDLER /////////
+    
+    ////// set player name /////////
     const handelForm = (e) => {
         e.preventDefault();
         setPlayer({ ...player,  name: name})
     }
 
+    ////// on Submitting Room Number make a call to server and get the status
     const handelForm2 = (e) => {
         setError("")
         e.preventDefault();
+        
+        // TODO : Make AJAX call instead
         socket.emit("tryToJoin", roomText);
         socket.on("resultJoiningRoom", (data)=>{
             if(!data.status){
@@ -27,8 +33,11 @@ export default function InfoPannel({ player, setPlayer, socket ,room, setRoom })
         })
     }
 
+    // on CREATE ROOM state change ask for random room id from server
     useEffect(() => {
         if(createRoom){
+            
+            // TODO : Make AJAX call instead
             socket.emit("getRoomId");
             socket.on('sendRoomId', (data)=>{
                 setRoom(data)
@@ -42,6 +51,7 @@ export default function InfoPannel({ player, setPlayer, socket ,room, setRoom })
                     <hr />
                     {player.name ? 
                         createRoom ?
+                         // If Player name is set(ie socket is set) and room is CREATE ROOM is set then show the ROOM ID
                             (
                                 <div className="players-name">
                                     Your Room Id is &nbsp; <br/>
@@ -53,23 +63,33 @@ export default function InfoPannel({ player, setPlayer, socket ,room, setRoom })
                                 </div>   
                             ):
                             joinRoom ?
+                             // If Player name is set(ie socket is set) and room is JOIN ROOM is set then show the form to fill the ROOM ID
                                 (
                                     <form className="players-name" onSubmit={handelForm2}>
                                         Enter The Room ID
                                         <br />
+                                            
+                                            // Input Form
                                         <input type="text" name="name" id="name" autoComplete="off" 
                                             autoFocus="true"
                                             pattern="[0-9]{6}"
                                             title="Room ID must be 6 digit number"
                                             value={roomText} onChange={(e)=>setRoomText(e.target.value)}/>
                                         <br />
+                                                
+                                                //buttons
+                                        // If any error then show the error
                                         {error && <strong style={{color :"#dd3300", fontSize: "15px"}}>{error}</strong>}
                                         <br />
                                         <button type="submit">Submit</button>
+
+                                        // on go back  room is disabled
                                         <button onClick={()=>{setRoom(); setJoinRoom(false); setCreateRoom(false); setRoomText(); setError("")}}
                                                 style={{backgroundColor :"#80818a"}}>Go Back</button>
                                     </form>
                                 ):
+                                
+                                // if Only Player name is set then ask them to join or create room
                                 (<div className="players-name">
                                     {name}, Ready To Play?
                                     <br />
@@ -77,6 +97,7 @@ export default function InfoPannel({ player, setPlayer, socket ,room, setRoom })
                                     <button onClick={() => setJoinRoom(true)}>Join Room!</button>
                                 </div>)
                     :
+                    // if Player name is not set then show the form to fill the name
                     <form className="players-name" onSubmit={handelForm} >
                             Enter Your name
                             <br />
