6d16b6ae6622e0eedd10e490f21b2f6782cc9a00
Pranava Adiga <66902872+adigapranava@users.noreply.github.com>
2021-09-22 17:41:26 +0530

    Create LeftGameBoard.js

--- FrontEnd/src/Components/LeftGameBoard.js
+++ FrontEnd/src/Components/LeftGameBoard.js
@@ -2,30 +2,41 @@ import React, { useEffect, useState } from 'react'
 import styles from './CSS/GameLeft.module.css'
 
 function LeftGameBoard({player, opponent, setPlayer, setOpponent,socket, started, room}) {
+    ////////////////////////////////////// VARIABLES ///////////////////////////
+    var BOARD;
     const VEL = 1;
     const BULLETS_VEL = 1.25;
     const MAX_BULLETS = 3;
+    
+    //// PLAYERS VAR
     var mySpace;
-    var hisSpace;
-    var BOARD;
     var myBullets = []
-    var hisBullets = []
-
     const [keysPressed, setKeysPressed] = useState({ArrowLeft: false, ArrowRight: false, ArrowUp: false, ArrowDown: false})
     var dummyKeysPressed = {...keysPressed}
-    var otherKeys
-
-    const [shoot, setShoot] = useState(false)
     const [myHearts, setmyHearts] = useState(player.hearts)
-    const [hisHearts, setHisHearts] = useState(opponent.hearts)
+    const [shoot, setShoot] = useState(false)
     var dummyShot = false;
+    
+    //// Other Players VAR
+    var hisSpace;
+    var hisBullets = []    
+    var otherKeys
+    const [hisHearts, setHisHearts] = useState(opponent.hearts)
+    
+    //To avoid buggy 1st Bullet Shot : variable that checks if any shoot by player
     const [anyShoots, setAnyShoots] = useState(false)
+    
+    // Arrow keys are allowed
     const allowedKeyPress = ["ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown"]
 
+    // TO handel FPS
     let lastTimestamp = 0,
     maxFPS = 60,
     timestep = 1000 / maxFPS;
 
+    ////////////////////////////////////// FUNCTIONS ///////////////////////////
+    
+    // Utility Functions
     const getWidthPersentage = (val)=>{
         return val / document.documentElement.clientWidth * 100
     }
@@ -34,6 +45,7 @@ function LeftGameBoard({player, opponent, setPlayer, setOpponent,socket, started
         return val / document.documentElement.clientHeight * 100
     }
 
+    // Ship movement Handler
     const handelMySpaceShipMovement = () =>{
         // LEFT
         if (dummyKeysPressed["ArrowLeft"] && getWidthPersentage(mySpace.offsetLeft) - VEL > 0) {
@@ -85,7 +97,53 @@ function LeftGameBoard({player, opponent, setPlayer, setOpponent,socket, started
             hisSpace.style.top = (getHeightPersentage(hisSpace.offsetTop) + VEL) + "%"
         }
     }
-
+    
+    const handelMyBullets = () => {
+         myBullets.forEach((bullet, index, object) => {
+                // for all bullets which exited the screen remove from board and `myBullets`
+                if(getWidthPersentage(bullet.offsetLeft) + BULLETS_VEL > 98){
+                    BOARD.removeChild(bullet);
+                    object.splice(index, 1);
+                }else{
+                    // if bullet hit the opponent then
+                    //      reduce his heart
+                    //      remove the bullet
+                    if (didBulletHitHim(bullet)) {
+                        setHisHearts(prevState => prevState - 1)
+                        BOARD.removeChild(bullet);
+                        object.splice(index, 1);
+                    }else{
+                        // else move the bullet
+                        bullet.style.left = getWidthPersentage(bullet.offsetLeft) + BULLETS_VEL + "%";
+                    }
+                }
+            });
+    }
+    
+    const handelHisBullets = () => {
+        hisBullets.forEach((bullet, index, object) => {
+                // for all bullets which exited the screen remove from board and `hisBullets`
+                if(getWidthPersentage(bullet.offsetLeft) < 0){
+                    BOARD.removeChild(bullet);
+                    object.splice(index, 1);
+                }else{
+                    // if bullet hit the player then
+                    //      reduce players heart
+                    //      remove the bullet
+                    if(didBulletHitMe(bullet)){
+                        setmyHearts(prevState => prevState - 1)
+                        BOARD.removeChild(bullet);
+                        object.splice(index, 1);
+                    }else{
+                        // else move the bullet
+                        bullet.style.left = getWidthPersentage(bullet.offsetLeft) - BULLETS_VEL + "%";
+                    }
+                }
+            });
+    }
+    
+    // CREATE BULLET
+    
     const createMyBullet = () =>{
         const bullet = document.createElement("div")
         bullet.setAttribute("class", styles.myBullets)
@@ -101,6 +159,8 @@ function LeftGameBoard({player, opponent, setPlayer, setOpponent,socket, started
         bullet.style.top = getHeightPersentage(hisSpace.offsetTop) + getHeightPersentage(hisSpace.offsetHeight) / 2 - 2 + "%"
         return bullet;
     }
+    
+    // COLLISION CHECK
 
     const didBulletHitMe = (bullet)=>{
         if(getWidthPersentage(bullet.offsetLeft)  < getWidthPersentage(mySpace.offsetLeft) + getWidthPersentage(mySpace.offsetWidth) 
@@ -124,12 +184,15 @@ function LeftGameBoard({player, opponent, setPlayer, setOpponent,socket, started
         }
     }
 
+    // On key press 
     const keyDown = (e) =>{
+        // if key is in ALLOWED KEYS
         if(allowedKeyPress.includes(e.key)){
-            if(dummyKeysPressed[e.key] === false){
+            if(dummyKeysPressed[e.key] === false){ // this avoids change of state simply even if state is true this avoids call to server whn state change occurs
                     dummyKeysPressed = {...keysPressed, [e.key]: true}
                     setKeysPressed(dummyKeysPressed)
             }
+            // if SPACE is pressed  and key is released (to avoid continious shooting if SPACE BAR is pressed and not realeased &&  bullets in the screen < MAX_BULLETS
         }else if(e.code === "Space" && dummyShot === false && myBullets.length < MAX_BULLETS){
             dummyShot = true;
             setAnyShoots(true)
@@ -149,6 +212,9 @@ function LeftGameBoard({player, opponent, setPlayer, setOpponent,socket, started
         }
     }
 
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////  ANIMATION RESPONSIBLE FUNCTION  ///////////////////////////////////////////////
+    
     const update = (timestamp)=>{
         window.requestAnimationFrame(update);
 
@@ -160,66 +226,49 @@ function LeftGameBoard({player, opponent, setPlayer, setOpponent,socket, started
         if(started){           
 
             handelMySpaceShipMovement()
-            handelOtherSpaceShipMovement()    
-
-            myBullets.forEach((bullet, index, object) => {
-                if(getWidthPersentage(bullet.offsetLeft) + BULLETS_VEL > 98){
-                    BOARD.removeChild(bullet);
-                    object.splice(index, 1);
-                }else{
-                    if (didBulletHitHim(bullet)) {
-                        setHisHearts(prevState => prevState - 1)
-                        BOARD.removeChild(bullet);
-                        object.splice(index, 1);
-                    }else{
-                        bullet.style.left = getWidthPersentage(bullet.offsetLeft) + BULLETS_VEL + "%";
-                    }
-                }
-            });
-
-            hisBullets.forEach((bullet, index, object) => {
-                if(getWidthPersentage(bullet.offsetLeft) < 0){
-                    BOARD.removeChild(bullet);
-                    object.splice(index, 1);
-                }else{
-                    if(didBulletHitMe(bullet)){
-                        setmyHearts(prevState => prevState - 1)
-                        BOARD.removeChild(bullet);
-                        object.splice(index, 1);
-                    }else{
-                        bullet.style.left = getWidthPersentage(bullet.offsetLeft) - BULLETS_VEL + "%";
-                    }
-                }
-            });
+            handelOtherSpaceShipMovement()
+            handelMyBullets()
+            handelHisBullets()
         }
     }
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+    // whn all components are rendered 
     useEffect(() => {
         otherKeys = {ArrowLeft: false, ArrowRight: false, ArrowUp: false, ArrowDown: false}
 
+        // grab myspace hisspace and board
         mySpace = document.getElementById(styles.mySpaceShip)
         hisSpace = document.getElementById(styles.enemySpaceShip)
         BOARD = document.getElementById("board")
         
+        // add key down up listener
         document.addEventListener("keydown", keyDown)
         document.addEventListener("keyup", keyUp)
 
+        // lister for the other players movement
         socket.on("heMoved", (movedKeys) => {
             otherKeys = movedKeys
         })
+        
+        // listen for other players shoot
         socket.on("heShot",()=>{
             // console.log("SSSS");
             var bullet = createHisBullet()
             hisBullets.push(bullet)
             BOARD.appendChild(bullet)
         })
+        
+        // call animation function
         requestAnimationFrame(update)
     }, [])
-
+    
+    // any change in KeysPressed state send to server
     useEffect(() => {
         socket.emit("iMoved", keysPressed, room);
     }, [keysPressed])
-
+    
+    // if shoot? then send to server
     useEffect(() => {
         if(anyShoots)
             socket.emit("iShot", room)
