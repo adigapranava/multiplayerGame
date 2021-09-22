aa9ec559bdf8679858a6c66716f953944aa26c4a
Pranava Adiga <66902872+adigapranava@users.noreply.github.com>
2021-09-22 13:36:28 +0530

    Update BeforeStart.js

--- FrontEnd/src/Components/BeforeStart.js
+++ FrontEnd/src/Components/BeforeStart.js
@@ -5,6 +5,7 @@ import GameInfoPannel from './GameInfoPannel'
 function BeforeStart({socket, room, player, setRoom, setOpponent, opponent, setPlayer}) {
     return (
         <>
+        // if opponent found and game not started then show the GAME WILL START SOON PAGE (ie <GameInfoPannel />)
         {opponent.name? <GameInfoPannel
             socket={socket}
             room={room}
